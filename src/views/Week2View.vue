3065c41951d90818fb3326c0745c7b9507a46591
Sandra <a0960529965@gmail.com>
2024-08-14 00:27:01 +0800

    signout

--- src/views/Week2View.vue
+++ src/views/Week2View.vue
@@ -15,6 +15,8 @@ const signupButton = async () => {
     signupRes.value = res.data.uid
   } catch (error) {
     console.log(error)
+    // alert('驗證失敗: ' + error.message);
+    alert('驗證失敗: ' + error.response.data.message);
   }
 }
 //登入
@@ -30,29 +32,45 @@ const signinButton = async () => {
     //寫入：cookie =token;到期日
     document.cookie = `todolistCookieName=${res.data.token}`
   } catch (error) {
-    console.log(error)
+    alert('驗證失敗: ' + error.message);
   }
 }
 //驗證登入
+const myToken = document.cookie.replace(
+    /(?:(?:^|.*;\s*)todolistCookieName\s*\=\s*([^;]*).*$)|^.*$/,
+    '$1'
+  )
 const user = ref({
   nickname: '',
   uid: ''
 })
 onMounted(async () => {
-  //讀出
-  var myToken = document.cookie.replace(
-    /(?:(?:^|.*;\s*)todolistCookieName\s*\=\s*([^;]*).*$)|^.*$/,
-    '$1'
-  )
-  console.log(myToken)
-  const res = await axios.post(`${api}/users/checkout`, {
+  try {
+      const res = await axios.get(`${api}/users/checkout`, {
     headers: {
       authorization: myToken
     }
   })
-  console.log(res)
   user.value = res.data
+  } catch (error) {
+    alert('驗證失敗: ' + error.message);
+  }
 })
+//登出
+
+const signoutButton = async () => {
+  try {
+    const res = await axios.post(`${api}/users/sign_out`,{},{
+      headers: {
+        authorization: myToken
+      }
+    })
+    console.log(res)
+    alert('登出: ' + res.data.message);
+  } catch (error) {
+    alert('失敗: ' + error.message);
+  }
+}
 </script>
 <template>
   <div class="week2">
@@ -70,6 +88,11 @@ onMounted(async () => {
     <button type="button" @click="signinButton">登入</button>
     {{ signIn }}
     token:{{ signinRes }}
+    {{ user }}
+    <h2>登出</h2>
+    <button type="button" @click="signoutButton">登出</button>
+    
+
   </div>
 </template>
 
